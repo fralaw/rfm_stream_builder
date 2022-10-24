0126b0c272dae21d98e4f3f2ab9d12388037534c
fralaw <f.luce4@studenti.uniba.it>
2022-10-24 22:50:18 +0200

    Modificata query in DBConnector per ottenere dati ordinati e con l'informazione sulle colonne

--- src/DBConnector.py
+++ src/DBConnector.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import pandas as pd
+import datetime
 
 """
 // Name        : DBConnector.py
@@ -33,6 +34,9 @@ class DBConnector:
     # Metodo che effettua la query estraendo le ricevute del giorno
     def extractReceipts(self, gg):
         cursor = self.__mydb.cursor()
-        cursor.execute("SELECT * FROM Receipts WHERE DATE(T_Receipt) = DATE(%s)", [gg.isoformat()])
+        cursor.execute("SELECT * FROM Receipts WHERE DATE(T_Receipt) = DATE(%s) ORDER BY K_Member, T_Receipt ASC", [gg.isoformat()])
         rows = cursor.fetchall()
-        return pd.DataFrame(rows)
+        columns = ["K_Receipt","K_Member","Quantity","Q_Amount","T_Receipt","Q_Discount_Amount"]
+        df = pd.DataFrame(rows)
+        df.columns = columns
+        return df
