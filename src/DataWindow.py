419f27b4595aa08675ca8d5321fc299ad5685fb8
fralaw <f.luce4@studenti.uniba.it>
2022-10-25 23:30:19 +0200

    Creata prima versione metodo generateExamples

--- src/DataWindow.py
+++ src/DataWindow.py
@@ -1,24 +1,29 @@
 import datetime as dt
+from itertools import islice
 
 import pandas as pd
 
 from CustomerWindow import CustomerWindow
 from DBConnector import DBConnector
 from Day import Day
+from Example import Example
+from ExampleDictionary import ExampleDictionary
 from Receipt import Receipt
+from Rfm import Rfm
 
 
 class DataWindow:
-    __dim = int()
-    __periods = int()
 
     def __init__(self, dim: int, periods: int):
-        self.__dim = dim
-        self.__periods = periods
+        self.__dim: int = dim
+        self.__periods: int = periods
         self.__currentDay: dt.date = None
+        self.__examples: ExampleDictionary = ExampleDictionary()
         self.__window: dict = {}
 
-    def set(self, data: pd.DataFrame, index: int = __dim * __periods):
+    def set(self, data: pd.DataFrame, index: int | None = None):
+        if index is None:
+            index = (self.__dim * self.__periods) - 1
         self.__currentDay = data.iloc[0]["T_Receipt"].to_pydatetime().date()
         receipts = []
         for i in range(0, data.shape[0] - 1):
@@ -45,7 +50,7 @@ class DataWindow:
                     self.__window[K_Member].setDay(day, lastPurchase, index)
                 receipts.clear()
 
-        row = data.iloc[data.shape[0]-1]
+        row = data.iloc[data.shape[0] - 1]
         K_Receipt = row["K_Receipt"]
         K_Member = row["K_Member"]
         Quantity = row["Quantity"]
@@ -72,15 +77,44 @@ class DataWindow:
 
     def deleteLatestDay(self):
         for elem in self.__window.items():
-            elem[0].deleteLatestDay()
+            elem[1].deleteLatestDay()
 
+    def generateExamples(self):
 
-        
+        for elem in self.__window.items():
+            example = Example(self.__currentDay)
+            days = elem[1].getListOfDays()
+            iter_days = iter(days)
+            length_to_split = [self.__dim] * self.__periods
+            periods = [list(islice(iter_days, elem))
+                       for elem in length_to_split]
+
+            for period in periods:
+                recency = 0
+                frequency = 0
+                monetary = 0
+                for day in period:
+                    if day is not None:
+                        for receipt in day.getReceiptsOfDay():
+                            recency = len(period) - period.index(day) - 1
+                            frequency += 1
+                            monetary += receipt.getQAmount()
+                            rfm = Rfm(recency, frequency, monetary)
+                            example.addExample(rfm)
+                            self.__examples.insertExample(elem[0], example)
+                    else:
+                        recency = len(period) - period.index(day) - 1
+                        rfm = Rfm(recency, frequency, monetary)
+                        example.addExample(rfm)
+                        self.__examples.insertExample(elem[0], example)
+
 
 
 
 dw = DataWindow(7, 4)
 db = DBConnector(password="Cicciopazzo98")
 df = db.extractReceipts(dt.date(2009, 1, 12))
-print(df.tail(2))
 dw.set(df, 1)
+dw.set(df, 15)
+
+dw.generateExamples()
