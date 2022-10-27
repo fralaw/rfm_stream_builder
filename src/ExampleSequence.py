7dfee700db444e08e4273611ec27a17eb47e9fe5
Andrea Antonio Brunetta <38211557+AndreaAntonio@users.noreply.github.com>
2022-10-27 19:18:03 +0200

    Revisionato metodo record di ExampleSequence e testato.

--- src/ExampleSequence.py
+++ src/ExampleSequence.py
@@ -4,8 +4,11 @@
 // Version     : 1.0
 // Description : Classe che modella la sequenza di esempi. E' formata da un unico attributo: la lista di Examples
 """
+import csv
 
 from Example import Example
+import datetime as dt
+from Rfm import Rfm
 
 
 class ExampleSequence:
@@ -19,21 +22,8 @@ class ExampleSequence:
     def appendExample(self, ex: Example):
         self.__examples.append(ex)
 
+    # Metodo record per etichettare e inserire nello stream.
     def record(self, label: bool, date, stream):
-        if label is True:
-            for ex in self.__examples:
-                description = ex.getDesc()
-                ex.setLabelTimeStamp(date)
-                for rfm in description:
-                    row = [str(rfm), str(ex.getLabelTimeStamp()), str(label)]
-                    stream.writerow(row)
-                self.__examples.clear()
-        else:
-            if len(self.__examples) > 1:
-                for i in range(0, len(self.__examples) - 2):
-                    ex = self.__examples[i]
-                    description = ex.getDesc()
-                    ex.setLabelTimeStamp(date)
-                    for rfm in description:
-                        row = [str(rfm), str(ex.getLabelTimeStamp()), str(label)]
-                        stream.writerow(row)
+        examplesList = [[str(ex.getDesc()), date, str(label)] for ex in self.__examples]
+        stream.writerows(examplesList)
+        self.__examples = []
