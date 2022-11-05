c7a5e9242eeb43dcf461fb5f0f1e49e3dbebaef6
Andrea Antonio Brunetta <38211557+AndreaAntonio@users.noreply.github.com>
2022-11-05 01:29:47 +0100

    Commentata classe ExampleSequence.

--- src/DBConnector.py
+++ src/DBConnector.py
@@ -1,5 +1,3 @@
-import mysql.connector
-
 """
 // Name        : DBConnector.py
 // Author      : Andrea Brunetta, Francesco Luce
@@ -10,6 +8,8 @@ import mysql.connector
                  MySQL Connector Python è scritto in Python ed è autosufficiente per eseguire query di database 
                  tramite Python. È un driver ufficiale supportato da Oracle per funzionare con MySQL e Python. 
 """
+
+import mysql.connector
 
 
 class DBConnector:
@@ -26,6 +26,7 @@ class DBConnector:
             - database: Il nome del database a cui si desidera connettersi ed eseguire le operazioni.
         Il costruttore setta gli attributi della classe con i parametri ricevuti in input.
     """
+
     def __init__(self, host="localhost", username="root", password="", database="brazilian_churn_retail_db"):
         self.__host = host
         self.__username = username
@@ -41,6 +42,7 @@ class DBConnector:
     """
         Metodo per chiudere le connessioni aperte al termine del lavoro.
     """
+
     def closeConnection(self):
         self.__mydb.close()
 
@@ -49,6 +51,7 @@ class DBConnector:
         I metodi execute() eseguono la query SQL e restituiscono il risultato.
         cursor() di un oggetto MySQLConnection per creare un oggetto cursore per eseguire varie operazioni SQL.
     """
+
     def extractReceipts(self, gg):
         cursor = self.__mydb.cursor()
         cursor.execute("SELECT * FROM Receipts WHERE DATE(T_Receipt) = %s ORDER BY K_Member, T_Receipt ASC",
@@ -60,6 +63,7 @@ class DBConnector:
         Metodo che effettua la query per estrarre la prima data del DB.
         MIN perchè è la data meno recente di tutto il DB.
     """
+
     def extractFirstDay(self):
         cursor = self.__mydb.cursor()
         cursor.execute("SELECT DATE(MIN(T_Receipt)) FROM Receipts")
@@ -70,6 +74,7 @@ class DBConnector:
             Metodo che effettua la query per estrarre l'ultima data del DB.
             MAX perchè è la data più recente di tutto il DB.
     """
+
     def extractLastDay(self):
         cursor = self.__mydb.cursor()
         cursor.execute("SELECT DATE(MAX(T_Receipt)) FROM Receipts")
--- src/ExampleSequence.py
+++ src/ExampleSequence.py
@@ -1,10 +1,9 @@
 """
 // Name        : ExampleSequence.py
 // Author      : Andrea Brunetta, Francesco Luce
-// Version     : 1.0
-// Description : Classe che modella la sequenza di esempi. E' formata da un unico attributo: la lista di Examples
+// Version     : 3.0
+// Description : Classe che modella la sequenza di esempi. È formata da un unico attributo: la lista di Examples
 """
-import csv
 
 from Example import Example
 import datetime as dt
@@ -14,15 +13,21 @@ from Rfm import Rfm
 class ExampleSequence:
     __examples: list[Example]
 
-    # Costruttore: inizializza la lista example come lista vuota
+    """ 
+        Metodo costruttore. Inizializza la lista di esempi come lista vuota.
+    """
     def __init__(self):
         self.__examples = []
 
-    # Metodo append per aggiungere un nuovo esempio alla sequenza di esempi
+    """
+    Metodo append per inserire nuovi esempi alla lista di esempi.
+    """
     def appendExample(self, ex: Example):
         self.__examples.append(ex)
 
-    # Metodo record per etichettare e inserire nello stream.
+    """
+        Metodo record per etichettare e mettere nello stream gli esempi.
+    """
     def record(self, label: bool, date, stream, customer):
         examplesList = [[str(ex.getDesc()), date, str(label), customer] for ex in self.__examples]
         stream.writerows(examplesList)
